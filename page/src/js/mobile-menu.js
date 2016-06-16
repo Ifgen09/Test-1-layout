6c66e1e099cf06cb417649fb899aa141f150a112
Kutnayk Evgeniy <ifgen09@gmail.com>
2016-06-16 14:50:31 +0300

    mobile menu

--- page/src/js/mobile-menu.js
+++ page/src/js/mobile-menu.js
@@ -1,15 +1,14 @@
 ;var MobileMenu = (function() {
 	'use strict';
 
-	// check window width without scroll width
-	var isMobile = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) < 768;
-
 	$('.main-nav__item-link').on('click', function(e) {
+		// check window width without scroll width
+		var isMobile = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) < 768;
 
 		if(isMobile) {
 			if($(this).next().hasClass('main-nav_submenu')) {
 				e.preventDefault();
-				$(this).next().slideToggle();
+				$(this).next().toggleClass('submenu-active');
 			}
 		}
 
